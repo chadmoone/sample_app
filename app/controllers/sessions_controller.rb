ec3938bd0f3a52fcb9b51f7d98b90476bbe75589
Chad Moone <chadmoone@gmail.com>
2010-11-14 20:58:14 -0500

    working on sessions and sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,8 @@ class SessionsController < ApplicationController
       flash.now[:error] = "Invalid email/password combination."
       render 'new'
     else
-      #handle login
+      sign_in user
+      redirect_to user
     end
   end
   
